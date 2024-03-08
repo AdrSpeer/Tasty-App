a51e555901f870e38036b748e8ad022ec144ea2d
AdrSpeer <adn.speer@gmail.com>
2024-03-08 14:32:54 +0100

    added Searchbar

--- src/Components/Searchbar/Searchbar.jsx
+++ src/Components/Searchbar/Searchbar.jsx
@@ -1,9 +1,36 @@
 import "./Searchbar.css";
+import React, { useEffect, useState } from "react";
+
+import SearchIcon from "@mui/icons-material/Search";
 const Searchbar = () => {
-    return (
-        <>
-        </>
-    );
-}
- 
+  const [placeholderVisible, setPlaceholderVisible] = useState(true);
+  const handleFocus = () => {
+    setPlaceholderVisible(false);
+  };
+
+  const Fetch = () => {
+    const [searchData, setSearchData] = useState([]);
+    useEffect(() => {
+      fetch("www.themealdb.com/api/json/v1/1/search.php?f=a")
+        .then((res) => res.json())
+        .then((data) => setSearchData(data))
+        .catch((err) => console.log("Fehler beim Laden der API", err));
+    }, []);
+  };
+
+  return (
+    <section className="searchbar">
+      <div className="searchbar-box">
+        <SearchIcon />
+        <input
+          name="searchbar"
+          type="text"
+          onFocus={handleFocus}
+          placeholder={placeholderVisible ? "Search" : ""}
+        />
+      </div>
+    </section>
+  );
+};
+
 export default Searchbar;
