19483b197ba68f0c28360cdc117f7c07c118de77
Nostea <kiwiuchi@googlemail.com>
2024-03-08 14:51:22 +0100

    added areas component with placeholder content, added Areas component to Home.jsx

---
+++ src/Components/Areas/Areas.jsx
@@ -0,0 +1,22 @@
+import SeeAll from "../SeeAll/SeeAll";
+import "./Areas.css";
+
+const Areas = () => {
+  return (
+    <>
+      <section className="areas-section">
+        <div className="areas-title-flex">
+          <h2>Areas</h2>
+          <SeeAll />
+        </div>
+        <div className="areas-filter-buttons">
+          <button>American</button>
+          <button>British</button>
+          <button>Dutch</button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Areas;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import "./Home.css";
 import MealOfTheDay from "../../Components/MealOfTheDay/MealOfTheDay";
+import Areas from "../../Components/Areas/Areas";
 const Home = () => {
   return (
     <>
       <MealOfTheDay MealTitle="Full English Breakfast" TagText="Breakfast" AreasText="British" />
+      <Areas />
     </>
   );
 };
