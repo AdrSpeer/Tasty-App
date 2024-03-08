b006b0b1457211b47634a4b2f3d4b1859724a4fd
StefanHolz1997 <Stefanholz97@web.de>
2024-03-08 15:29:56 +0100

    FCS_Design_Finish_2

--- src/Components/FoodCardSmall/FoodCardSmall.jsx
+++ src/Components/FoodCardSmall/FoodCardSmall.jsx
@@ -3,9 +3,11 @@ const FoodCardSmall = () => {
   return (
     <section className="fcs-container">
       <div className="fcs-image">
+        {/* Image: "strMealThumb" aus Fetch fehlt */}
         <img src="../../../public/fcs-test-image.png" alt="Food-Image" />
       </div>
       <div className="fcs-name">
+        {/* Name: "strMeal" aus Fetch fehlt */}
         <p>Piri-piri chicken and slaw</p>
       </div>
     </section>
