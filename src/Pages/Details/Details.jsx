8b38a43754264d4d14a27edcf81c582032515edb
StefanHolz1997 <Stefanholz97@web.de>
2024-03-11 16:21:09 +0100

    Details-Instructions-Import

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -28,67 +28,72 @@ const Details = () => {
                 <button>Ingredients</button>
                 <button>Instructions</button>
               </div>
-              <h5>Ingredients</h5>
-              <p>
-                {item.strMeasure1} {item.strIngredient1}
-              </p>
-              <p>
-                {item.strMeasure2} {item.strIngredient2}
-              </p>
-              <p>
-                {item.strMeasure3} {item.strIngredient3}
-              </p>
-              <p>
-                {item.strMeasure4} {item.strIngredient4}
-              </p>
-              <p>
-                {item.strMeasure5} {item.strIngredient5}
-              </p>
-              <p>
-                {item.strMeasure6} {item.strIngredient6}
-              </p>
-              <p>
-                {item.strMeasure7} {item.strIngredient7}
-              </p>
-              <p>
-                {item.strMeasure8} {item.strIngredient8}
-              </p>
-              <p>
-                {item.strMeasure9} {item.strIngredient9}
-              </p>
-              <p>
-                {item.strMeasure10} {item.strIngredient10}
-              </p>
-              <p>
-                {item.strMeasure11} {item.strIngredient11}
-              </p>
-              <p>
-                {item.strMeasure12} {item.strIngredient12}
-              </p>
-              <p>
-                {item.strMeasure13} {item.strIngredient13}
-              </p>
-              <p>
-                {item.strMeasure14} {item.strIngredient14}
-              </p>
-              <p>
-                {item.strMeasure15} {item.strIngredient15}
-              </p>
-              <p>
-                {item.strMeasure16} {item.strIngredient16}
-              </p>
-              <p>
-                {item.strMeasure17} {item.strIngredient17}
-              </p>
-              <p>
-                {item.strMeasure18} {item.strIngredient18}
-              </p>
-              <p>
-                {item.strMeasure19} {item.strIngredient19}
-              </p>
-              <p>
-                {item.strMeasure20} {item.strIngredient20}
-              </p>
+              <div className="ingredients">
+                <h5>Ingredients</h5>
+                <p>
+                  {item.strMeasure1} {item.strIngredient1}
+                </p>
+                <p>
+                  {item.strMeasure2} {item.strIngredient2}
+                </p>
+                <p>
+                  {item.strMeasure3} {item.strIngredient3}
+                </p>
+                <p>
+                  {item.strMeasure4} {item.strIngredient4}
+                </p>
+                <p>
+                  {item.strMeasure5} {item.strIngredient5}
+                </p>
+                <p>
+                  {item.strMeasure6} {item.strIngredient6}
+                </p>
+                <p>
+                  {item.strMeasure7} {item.strIngredient7}
+                </p>
+                <p>
+                  {item.strMeasure8} {item.strIngredient8}
+                </p>
+                <p>
+                  {item.strMeasure9} {item.strIngredient9}
+                </p>
+                <p>
+                  {item.strMeasure10} {item.strIngredient10}
+                </p>
+                <p>
+                  {item.strMeasure11} {item.strIngredient11}
+                </p>
+                <p>
+                  {item.strMeasure12} {item.strIngredient12}
+                </p>
+                <p>
+                  {item.strMeasure13} {item.strIngredient13}
+                </p>
+                <p>
+                  {item.strMeasure14} {item.strIngredient14}
+                </p>
+                <p>
+                  {item.strMeasure15} {item.strIngredient15}
+                </p>
+                <p>
+                  {item.strMeasure16} {item.strIngredient16}
+                </p>
+                <p>
+                  {item.strMeasure17} {item.strIngredient17}
+                </p>
+                <p>
+                  {item.strMeasure18} {item.strIngredient18}
+                </p>
+                <p>
+                  {item.strMeasure19} {item.strIngredient19}
+                </p>
+                <p>
+                  {item.strMeasure20} {item.strIngredient20}
+                </p>
+              </div>
+              <div className="instructions">
+                <p>{item.strInstructions}</p>
+              </div>
             </div>
           </section>
         ))
