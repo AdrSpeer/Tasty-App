19eb859204161578a33cfde8142f30a5fb4c6821
AdrSpeer <adn.speer@gmail.com>
2024-03-11 15:12:01 +0100

    added categoriesPage

--- src/Pages/CategoriesPage/CategoriesPage.jsx
+++ src/Pages/CategoriesPage/CategoriesPage.jsx
@@ -1,9 +1,34 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import "./CategoriesPage.css";
 const CategoriesPage = () => {
-    return (
-        <>
-        </>
-    );
-}
- 
+  const [category, setCategory] = useState(null);
+  const { categories } = useParams();
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categories}`)
+      .then((res) => res.json())
+      .then((categoryData) => setCategory(categoryData))
+      .catch((error) => console.error("Fehler :-(", error));
+  }, []);
+
+  console.log(category);
+
+  return (
+    <section className="categories-page">
+      {category?.length > 0 ? (
+        category?.map((item, index) => (
+          <div className="cat-filter" key={index}>
+            <Link to={`/categorie/${item.strCategory}`}>
+              <img src={item.strCategoryThumb} alt="category IMG" />
+              <p>{item.strCategory}</p>
+            </Link>
+          </div>
+        ))
+      ) : (
+        <p>Loading ...</p>
+      )}
+    </section>
+  );
+};
+
 export default CategoriesPage;
