3cf39f4e7260056546fd2a84ec14744ba01641e4
StefanHolz1997 <Stefanholz97@web.de>
2024-03-11 13:23:08 +0100

    FCS-Fetch-Solution

--- src/Components/FoodCardSmall/FoodCardSmall.jsx
+++ src/Components/FoodCardSmall/FoodCardSmall.jsx
@@ -5,24 +5,23 @@ const FoodCardSmall = () => {
   const [areas, setAreas] = useState([]);
   const [categories, setCategories] = useState([]);
 
+  // useEffect(() => {
+  //   //   // Bei fetch nach "c=" noch den Button Value einfügen
+  //   //   fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef")
+  //   //   .then((res) => res.json())
+  //   //   .then((data) => setCategories(data))
+  //   //   .catch((err) => console.error("Fehler Bei FCS-Categories", err));
+  //   // }, []);
+
   useEffect(() => {
     // Bei fetch nach "a=" noch den Button Value einfügen
     fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=American")
       .then((res) => res.json())
-      .then((areaData) => setAreas(areaData))
+      .then((areaData) => setAreas(areaData.meals))
       .catch((err) => console.error("Fehler Bei FCS-Areas", err));
   }, []);
-
+  // console.log(categories);
   console.log(areas);
-  useEffect(() => {
-    // Bei fetch nach "c=" noch den Button Value einfügen
-    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef")
-      .then((res) => res.json())
-      .then((data) => setCategories(data))
-      .catch((err) => console.error("Fehler Bei FCS-Categories", err));
-  }, []);
-
-  console.log(categories);
 
   return (
     <section className="fcs-container">
